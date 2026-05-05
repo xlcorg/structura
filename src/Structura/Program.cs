65b424034031f2db1d9bac569f1b3d06d5de718a
xlcode <xlc.code@gmail.com>
2026-05-05 22:27:02 +0300

    Initial commit

---
+++ src/Structura/Program.cs
@@ -0,0 +1,13 @@
+﻿using Structura.Generated;
+using Structura.Runtime;
+using Structura.Reporting;
+
+string orderJson = File.ReadAllText("Samples/order.sample.json");
+var order = orderJson.ParseJson<OrderSampleJson>();
+
+order.Currency = "USD";
+
+var modifiedJson = order.ToJson();
+
+SimpleReporter.Print(order);
+ConsoleDiffReporter.Print(order);
